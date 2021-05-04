04db5bed1f27c3f774aadb61669df9647a4d1e58
SSong-develop <thdgnsrl123@naver.com>
2021-05-04 21:44:49 +0900

    border fix

--- app/src/main/java/com/ssong_develop/selectableshadowpositionview/SelectableShadowPositionView.kt
+++ app/src/main/java/com/ssong_develop/selectableshadowpositionview/SelectableShadowPositionView.kt
@@ -259,7 +259,7 @@ class SelectableShadowPositionView @JvmOverloads constructor(
         }
 
         borderRectF.apply {
-            top = height - borderHeight
+            top = 0f
             left = 0f
             right = width.toFloat()
             bottom = height.toFloat()
